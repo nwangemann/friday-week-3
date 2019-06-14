024527e5ded6f1cadfe9686bec17f940a5904879
Nate <nathan.wangemann@aggiemail.usu.edu>
2019-06-14 12:59:14 -0700

    add framework for new approach, no functionality

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,16 @@ $(document).ready(function(){
     var replacedArray = []
     console.log(arrayBase);
     console.log(newArray);
-    arrayBase.forEach(function(num, i) {
-      if ((parseInt(num)) % 10 === 3 || (parseInt(num)) === 3 || (parseInt(num)) >= 30 && (parseInt(num)) <= 39 || (parseInt(num)) >= 300 && (parseInt(num)) <= 399 || (parseInt(num)) >= 3000 && (parseInt(num)) <= 3999 || (parseInt(num)) >= 30000 && (parseInt(num)) <= 39999) {
+    arrayBase.forEach(function(string) {
+      var num = string
+      if (string.includes("3")) {
         console.log("Reached loop 1")
         replacedArray.push(arrayReplace[3]);
-      } else if ((parseInt(num)) % 10 === 2 || (parseInt(num)) === 2 || (parseInt(num)) >= 20 && (parseInt(num)) <= 29 || (parseInt(num)) >= 200 && (parseInt(num)) <= 299 || (parseInt(num)) >= 2000 && (parseInt(num)) <= 2999 || (parseInt(num)) >= 20000 && (parseInt(num)) <= 29999) {
+      } else if (string.includes("2")) {
         console.log("Reached loop 2")
         replacedArray.push(arrayReplace[2]);
         return
-      } else if ((parseInt(num)) % 10 === 1 || (parseInt(num)) === 1 || (parseInt(num)) >= 10 && (parseInt(num)) <= 19 || (parseInt(num)) >= 100 && (parseInt(num)) <= 199 || (parseInt(num)) >= 1000 && (parseInt(num)) <= 1999 || (parseInt(num)) >= 10000 && (parseInt(num)) <= 19999)  {
+      } else if (string.includes("1"))  {
         console.log("Reached loop 3")
         replacedArray.push(arrayReplace[1]);
         return
