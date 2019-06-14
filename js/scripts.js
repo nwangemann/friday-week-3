e1107eae4a54894197dfc2ecf46c38d9c26fb89f
Nate <nathan.wangemann@aggiemail.usu.edu>
2019-06-14 16:31:44 -0700

    clean up code, delete code no longer in use, fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
     });
     $(".output").text(finalArray);
     $(".output").fadeIn();
-    $(".buttonClass").fadeIn();
+    $(".buttonClass").fadeIn(3000);
     reverseArray = finalArray.reverse();
     $(".reverseOutput").text(finalArray);
     console.log(reverseArray);
@@ -36,7 +36,6 @@ $(document).ready(function(){
     });
   });
 });
-
 
 function evaluator(arrayItem) {
   var result = "";
@@ -49,9 +48,7 @@ function evaluator(arrayItem) {
   } else {
     return result = arrayItem;
   }
-
 }
-
 
 function arrayMaker(elementCount) {
   for (var counter = 1; counter <= elementCount; counter++) {
@@ -62,52 +59,3 @@ function arrayMaker(elementCount) {
     }
   }
 }
-//
-// $(document).ready(function(){
-  //   $(".form").submit(function(event) {
-    //     event.preventDefault();
-    //     var userInput = parseInt($("#input1").val());
-    //     var elementCount = userInput + 1
-    //     newArray = arrayMaker(elementCount);
-    //     var replacedArray = []
-    //     console.log(arrayBase);
-    //     console.log(newArray);
-    //     for (var index = 0; index > arrayBase.length; index++) {
-      //
-      //     }
-      //   });
-      //   // $(".output").text(storeArray);
-      // });
-      //
-
-      //
-      // functional but does not take 10-19 100-199 1000-1999 etc into account
-      // $(document).ready(function(){
-        //     $(".form").submit(function(event) {
-          //         event.preventDefault();
-          //         var userInput = parseInt($("#input1").val());
-          //         var elementCount = userInput + 1
-          //         newArray = arrayMaker(elementCount);
-          //         var replacedArray = []
-          //         console.log(arrayBase);
-          //         console.log(newArray);
-          //         arrayBase.forEach(function(num, i) {
-            //             if ((parseInt(num)) % 10 === 3 || (parseInt(num)) === 3 || (parseInt(num)) >= 30 && (parseInt(num)) <= 39 || (parseInt(num)) >= 300 && (parseInt(num)) <= 399 || (parseInt(num)) >= 3000 && (parseInt(num)) <= 3999 || (parseInt(num)) >= 30000 && (parseInt(num)) <= 39999) {
-              //                 console.log("Reached loop 1")
-              //                 replacedArray.push(arrayReplace[3]);
-              //               } else if ((parseInt(num)) % 10 === 2 || (parseInt(num)) === 2 || (parseInt(num)) >= 20 && (parseInt(num)) <= 29 || (parseInt(num)) >= 200 && (parseInt(num)) <= 299 || (parseInt(num)) >= 2000 && (parseInt(num)) <= 2999 || (parseInt(num)) >= 20000 && (parseInt(num)) <= 29999) {
-                //                   console.log("Reached loop 2")
-                //                   replacedArray.push(arrayReplace[2]);
-                //                   return
-                //                 } else if ((parseInt(num)) % 10 === 1 || (parseInt(num)) === 1 || (parseInt(num)) >= 10 && (parseInt(num)) <= 19 || (parseInt(num)) >= 100 && (parseInt(num)) <= 199 || (parseInt(num)) >= 1000 && (parseInt(num)) <= 1999 || (parseInt(num)) >= 10000 && (parseInt(num)) <= 19999)  {
-                  //                     console.log("Reached loop 3")
-                  //                     replacedArray.push(arrayReplace[1]);
-                  //                     return
-                  //                   } else {
-                    //                       replacedArray.push(num);
-                    //                     };
-                    //                     console.log(replacedArray);
-                    //                   });
-                    //                   $(".output").text(replacedArray);
-                    //                 });
-                    //               });
