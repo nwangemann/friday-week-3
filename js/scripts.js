bc3b54f64d3cdf0a4e65372d049aadc46cc3e8ff
Nate <nathan.wangemann@aggiemail.usu.edu>
2019-06-14 10:07:28 -0700

    add a function to take input and create an array with each number leading up to that number

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 var array1 = []
-
+var newArray = []
 $(document).ready(function(){
   $(".form").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#input1").val());
-    console.log(userInput);
-    $(".output").text(userInput);
+    var elementCount = userInput + 1
+    newArray = arrayMaker(elementCount);
+    $(".output").text(newArray);
+    console.log(array1);
+    console.log(elementCount);
 
   });
 });
+
+function arrayMaker(elementCount) {
+  for (var counter = 1; counter <= elementCount; counter++) {
+    if(counter !== elementCount) {
+      array1.push(counter);
+    } else {
+      return
+    }
+  }
+}
